2b91af0c5c2b96e3d50f165b7ecf0e175f5a821d
Marcelo Barsotti <mbarsott@gmail.com>
2019-03-30 16:38:37 -0400

    2. Arrays

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -14,20 +14,41 @@ namespace Acme.Biz
     public class Product
     {
         #region Constructors
+
         public Product()
         {
+//            var colorOptions = new string[4];
+//            colorOptions[0] = "Red";
+//            colorOptions[1] = "Espresso";
+//            colorOptions[2] = "White";
+//            colorOptions[3] = "Navy";
+            string[] colorOptions = {"Red", "Espresso", "White", "Navy"};
+            var brownIndex = Array.IndexOf(colorOptions, "Espresso");
+            colorOptions.SetValue("Blue", 3);
+            for (int i = 0; i < colorOptions.Length; i++)
+            {
+                colorOptions[i] = colorOptions[i].ToLower();
+            }
+
+            foreach (var color in colorOptions)
+            {
+                Console.WriteLine($"The color is {color}");
+            }
         }
+
         public Product(int productId,
-                        string productName,
-                        string description) : this()
+            string productName,
+            string description) : this()
         {
             this.ProductId = productId;
             this.ProductName = productName;
             this.Description = description;
         }
+
         #endregion
 
         #region Properties
+
         public DateTime? AvailabilityDate { get; set; }
 
         public decimal Cost { get; set; }
@@ -37,9 +58,11 @@ namespace Acme.Biz
         public int ProductId { get; set; }
 
         private string productName;
+
         public string ProductName
         {
-            get {
+            get
+            {
                 var formattedValue = productName?.Trim();
                 return formattedValue;
             }
@@ -52,24 +75,25 @@ namespace Acme.Biz
                 else if (value.Length > 20)
                 {
                     ValidationMessage = "Product Name cannot be more than 20 characters";
-
                 }
                 else
                 {
                     productName = value;
-
                 }
             }
         }
 
         private Vendor productVendor;
+
         public Vendor ProductVendor
         {
-            get {
+            get
+            {
                 if (productVendor == null)
                 {
                     productVendor = new Vendor();
                 }
+
                 return productVendor;
             }
             set { productVendor = value; }
@@ -85,7 +109,7 @@ namespace Acme.Biz
         /// <param name="markupPercent">Percent used to mark up the cost.</param>
         /// <returns></returns>
         public decimal CalculateSuggestedPrice(decimal markupPercent) =>
-             this.Cost + (this.Cost * markupPercent / 100);
+            this.Cost + (this.Cost * markupPercent / 100);
 
         public override string ToString()
         {
