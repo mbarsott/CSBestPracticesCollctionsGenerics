e21cb4fe3fcefad25f05c7d419570af5bda79c72
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-04-03 10:01:02 -0400

    5.3. Generic Dictionaries - Initializing a  Dictionary of Objects

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -35,34 +35,44 @@ namespace Acme.Biz
 
         public Product()
         {
-//            var colorOptions = new List<string>();
-//            colorOptions.Add("Red");
-//            colorOptions.Add("Espresso");
-//            colorOptions.Add("White");
-//            colorOptions.Add("Navy");
-//            colorOptions.Insert(2, "Purple");
-//            colorOptions.Remove("White");
+            #region Lists and Arrays
+            //            var colorOptions = new List<string>();
+            //            colorOptions.Add("Red");
+            //            colorOptions.Add("Espresso");
+            //            colorOptions.Add("White");
+            //            colorOptions.Add("Navy");
+            //            colorOptions.Insert(2, "Purple");
+            //            colorOptions.Remove("White");
             var colorOptions = new List<string>() {"Red", "Espresso", "White", "Navy"};
 
             Console.WriteLine(colorOptions);
 
-//            var colorOptions = new string[4];
-//            colorOptions[0] = "Red";
-//            colorOptions[1] = "Espresso";
-//            colorOptions[2] = "White";
-//            colorOptions[3] = "Navy";
-//            string[] colorOptions = {"Red", "Espresso", "White", "Navy"};
-//            var brownIndex = Array.IndexOf(colorOptions, "Espresso");
-//            colorOptions.SetValue("Blue", 3);
-//            for (int i = 0; i < colorOptions.Length; i++)
-//            {
-//                colorOptions[i] = colorOptions[i].ToLower();
-//            }
-//
-//            foreach (var color in colorOptions)
-//            {
-//                Console.WriteLine($"The color is {color}");
-//            }
+            //            var colorOptions = new string[4];
+            //            colorOptions[0] = "Red";
+            //            colorOptions[1] = "Espresso";
+            //            colorOptions[2] = "White";
+            //            colorOptions[3] = "Navy";
+            //            string[] colorOptions = {"Red", "Espresso", "White", "Navy"};
+            //            var brownIndex = Array.IndexOf(colorOptions, "Espresso");
+            //            colorOptions.SetValue("Blue", 3);
+            //            for (int i = 0; i < colorOptions.Length; i++)
+            //            {
+            //                colorOptions[i] = colorOptions[i].ToLower();
+            //            }
+            //
+            //            foreach (var color in colorOptions)
+            //            {
+            //                Console.WriteLine($"The color is {color}");
+            //            }
+            #endregion
+//            var states = new Dictionary<string, string>();
+//            states.Add("CA", "California");
+//            states.Add("WA", "Washington");
+//            states.Add("NY", "New York");
+            var states = new Dictionary<string, string>()
+                {{"CA", "California"}, {"WA", "Washington"}, {"NY", "New York"}};
+
+            Console.WriteLine(states);
         }
 
         public Product(int productId,
