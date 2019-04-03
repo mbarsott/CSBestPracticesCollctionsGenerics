11666fe8b2d56ca9d0016ce7a815d7c75e91683c
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-04-03 10:45:22 -0400

    5. Generic Dictionaries

--- AcmeApp/Acme.Biz/VendorRepository.cs
+++ AcmeApp/Acme.Biz/VendorRepository.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Acme.Biz
 {
@@ -11,13 +8,13 @@ namespace Acme.Biz
         private List<Vendor> vendors;
 
         /// <summary>
-        /// Retrieve one vendor.
+        ///     Retrieve one vendor.
         /// </summary>
         /// <param name="vendorId">Id of the vendor to retrieve.</param>
         public Vendor Retrieve(int vendorId)
         {
             // Create the instance of the Vendor class
-            Vendor vendor = new Vendor();
+            var vendor = new Vendor();
 
             // Code that retrieves the defined customer
 
@@ -32,19 +29,20 @@ namespace Acme.Biz
             return vendor;
         }
 
+        /// <summary>
+        /// Retrieves this instance.
+        /// </summary>
+        /// <returns></returns>
         public List<Vendor> Retrieve()
         {
             if (vendors == null)
             {
                 vendors = new List<Vendor>();
-                vendors.Add(new Vendor() {VendorId = 1, CompanyName = "ABC Corp", Email = "abc@abc.com"});
-                vendors.Add(new Vendor() {VendorId = 2, CompanyName = "XYZ Inc", Email = "xyz@xyz.com"});
+                vendors.Add(new Vendor {VendorId = 1, CompanyName = "ABC Corp", Email = "abc@abc.com"});
+                vendors.Add(new Vendor {VendorId = 2, CompanyName = "XYZ Inc", Email = "xyz@xyz.com"});
             }
 
-            for (int i = 0; i < vendors.Count; i++)
-            {
-                Console.WriteLine(vendors[i]);
-            }
+            for (var i = 0; i < vendors.Count; i++) Console.WriteLine(vendors[i]);
 
             foreach (var vendor in vendors)
             {
@@ -54,16 +52,52 @@ namespace Acme.Biz
             return vendors;
         }
 
+        public Dictionary<string, Vendor> RetrieveWithKeys()
+        {
+            var vendors = new Dictionary<string, Vendor>()
+            {
+                {"ABC Corp", new Vendor() {VendorId = 5, CompanyName = "ABC Corp", Email = "abc@abc.com"}},
+                {"XYZ Inc", new Vendor() {VendorId = 8, CompanyName = "XYZ Inc", Email = "xyz@xyz.com"}}
+            };
+            foreach (var element in vendors)
+            {
+                var vendor = element.Value;
+                var key = element.Key; 
+                Console.WriteLine($"Key: {key} Value: {vendor}");
+            }
+//            foreach (var vendor in vendors.Values)
+//            {
+//                Console.WriteLine(vendor);
+//            }
+//            foreach (var companyName in vendors.Keys)
+//            {
+//                Console.WriteLine(companyName);
+//                Console.WriteLine(vendors[companyName]);
+//            }
+//            Console.WriteLine(vendors["XYZ Inc"]);
+//            if (vendors.ContainsKey("XYZ"))
+//            {
+//                Console.WriteLine(vendors["XYX"]);
+//            }
+//
+//            Vendor vendor;
+//            if (vendors.TryGetValue("XYZ", out vendor))
+//            {
+//                Console.WriteLine(vendor);
+//            }
+            return vendors;
+        }
+
         public T RetrieveValue<T>(string sql, T defaultValue)
         {
             // Call the database to retrieve the value
             // If no value is returned, return the default value
-            T value = defaultValue;
+            var value = defaultValue;
             return value;
         }
 
         /// <summary>
-        /// Save data for one vendor.
+        ///     Save data for one vendor.
         /// </summary>
         /// <param name="vendor">Instance of the vendor to save.</param>
         /// <returns></returns>
